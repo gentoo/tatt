beac8a9b3b3f14eafb635862c0d2c56030d77b82
Thomas Kahle <tom111@gmx.de>
2010-01-16 18:55:21 +0100

    Skeleton

--- tatt.py
+++ tatt.py
@@ -1,22 +1,49 @@
 #!/usr/bin/python
 
-### USAGE and OPTIONS ###
-
-from optparse import OptionParser
-
-parser=OptionParser()
-parser.add_option("-d", "--depend", help="Determine stable rdeps")
-parser.add_option("-f", "--file", "-o", help="Outfile name")
-
-(options,args) = parser.parse_args()
-
-print options.file
-
 import sys
 import re
 import random
 from subprocess import *
 import os
+
+## Testing the validity of a package atom ##
+def atomtest(atom):
+    ### A VERY SIMPLE regular expression to test for a package atom ####
+    ### Can we use stuff from portage here?
+    testa = re.compile('=?\\w+.*/.*')
+    if testa.match(atom) == None :
+        print "Sorry, no valid package atom given"
+        print "Use the version string starting with = and ending in the version"
+        exit(1)
+############################################
+
+## Generate stable rdeps ###
+def stableredeps (atom):
+    """
+    Find packages with stable versions which depend on atom
+    """
+    pass
+#############################
+
+## Useflag Combis ##
+def findUseFlagCombis (atom):
+    """
+    Generate combinations of use flags to test
+    """
+    pass
+#####################
+
+######### Main program starts here ###############
+
+
+### USAGE and OPTIONS ###
+from optparse import OptionParser
+
+parser=OptionParser()
+parser.add_option("-d", "--depend", help="Determine stable rdeps")
+parser.add_option("-f", "--file", "-o", help="Outfile name")
+    
+(options,args) = parser.parse_args()
 
 if os.path.isfile(options.file):
     print ("WARNING! "+options.file+" exsits. I will overwrite it!")
@@ -28,60 +55,13 @@ if (Popen(['whoami'], stdout=PIPE).communicate()[0].rstrip() == 'root'):
 else:
     print "You're not root!"
     isroot=False
-
+    
 try:
-    atom = sys.argv[1]
+    atom = args[1]
 except IndexError:
     print "Please call with package atom as argument"
     exit (1)
 
-# A VERY SIMPLE regular expression to test for a package atom
-atomtest = re.compile('=?\\w+.*/.*')
-if atomtest.match(atom) == None :
-    print "Sorry, no valid package atom given"
-    print "Use the version string starting with = and ending in the version"
-    exit(1)
+print (options,args)
 
-# get the useflags
-uses=Popen('equery -C uses '+atom+' | cut -f 1 | cut -c 2-40 | xargs',shell=True,stdout=PIPE).communicate()[0]
-
-uselist=uses.split()
-
-# Here is my naive subset iterator
-# usecombis = [[ x for (pos,x) in zip(range(len(uselist)),uselist)
-#              if (2**pos) & switches] for switches in range(2**len(uselist))]
-
-usecombis=[]
-for sw in range(2**len(uselist)):
-    mod = []
-    for pos in range(len(uselist)):
-        if ((2**pos) & sw):
-            mod.append("")
-        else:
-            mod.append("-")
-    usecombis.append(zip(mod,uselist))
-
-usecombis = [["".join(uf) for uf in combi] for combi in usecombis]
-
-# If more than 5 useflags, pick 32 combis at random.
-if len(usecombis)>32:
-    random.seed()
-    usecombis = random.sample(usecombis, 32)
-
-for uc in usecombis:
-    ucs = " ".join(uc) # The string with USE+"..."
-    callstring = 'USE="'+ucs+'" emerge -1v '+'"'+atom+'"'
-    if isroot:
-        mes = ("Will now run : \n " + callstring)
-        outfile.write(mes)
-        print mes
-        retvalue = call(callstring,shell=True)
-        if retvalue > 0:
-            mes = "returnvalue > 0 !"
-            print mes
-            outfile.write(mes+"\n")
-        else:
-            outfile.write("\n ==> OK \n")
-        outfile.flush()
-    else:
-        print callstring
+## That's all folks ##
