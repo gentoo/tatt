bfcf55948db9a0837e5a1ff604e907fca2580183
Thomas Kahle <tom111@gmx.de>
2010-01-29 14:27:43 +0100

    Removing duplicates from the rdeplist before output.

--- tatt.py
+++ tatt.py
@@ -17,6 +17,23 @@ def atomtest(atom):
         exit(1)
 ############################################
 
+## Getting unique elements of a list ##
+def unique(seq, idfun=None):
+    # order preserving
+    if idfun is None:
+        def idfun(x): return x
+        seen = {}
+        result = []
+        for item in seq:
+            marker = idfun(item)
+            # in old Python versions:
+            # if seen.has_key(marker)
+            # but in new ones:
+            if marker in seen: continue
+            seen[marker] = 1
+            result.append(item)
+    return result
+
 ## Generate stable rdeps ###
 def stableredeps (atom):
     """
@@ -47,7 +64,7 @@ def stableredeps (atom):
         outlist.remove('')
         if len(outlist) == 0:
             return []
-    return outlist
+    return unique(outlist)
     
 #############################
 
