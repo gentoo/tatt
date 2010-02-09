34995fe256a0676f71604af60e7d25f09ff5b313
Thomas Kahle <tom111@gmx.de>
2010-02-09 18:35:05 +0100

    Handling of leading '=' and and missing version strings

--- gentooPackage.py
+++ gentooPackage.py
@@ -11,21 +11,28 @@ class gentooPackage(object):
     
     def __init__(self, st):
         """An atom is initialized from an atom string"""
+        if st[0] == '=': st = st[1:]
         slashparts = st.split("/")
         self.category = slashparts[0]
         minusparts = slashparts[1].split("-")
-        self.name = ""
-        while 1:
-            p = minusparts.pop(0)
-            # Try a number after a '-'
-            if re.match('[0-9]+', p):
-                # Version starts here:
-                self.ver = "-".join([p] + minusparts)
-                break
-            else:
-                # Append back to name
-                if self.name=="": self.name = p
-                else : self.name = "-".join([self.name, p])
+        self.ver = ""
+        if len (minusparts) == 1:
+            # No version given
+            self.name=slashparts[1]
+        else:
+            # Parse the name-version part
+            self.name=""
+            while 1:
+                p = minusparts.pop(0)
+                # Try a number after a '-'
+                if re.match('[0-9]+', p):
+                    # Version starts here:
+                    self.ver = "-".join([p] + minusparts)
+                    break
+                else:
+                    # Append back to name
+                    if self.name=="": self.name = p
+                    else : self.name = "-".join([self.name, p])
 
 
     def packageString(self):
@@ -35,3 +42,4 @@ class gentooPackage(object):
         else:
             return ( "=" + "/".join([self.category, "-".join([self.name, self.ver])]))
 
+        
