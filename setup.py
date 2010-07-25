0d202dd82c329765dc776e58f752e6ef9b6f796f
Thomas Kahle <tom111@gmx.de>
2010-07-25 20:26:09 +0200

    setup.py extended

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='tatt',
-      packages=['tatt']
+setup(name = "tatt",
+      version = "0.1",
+      packages = find_packages(),
+      scripts = ['tatt'],
+
+      install_requires = [''],
+
+      # metadata for upload to PyPI
+      author = "Thomas Kahle",
+      author_email = "tom111@gmx.de",
+      description = "tatt is an arch testing tool",
+      license = "GPL-2",
+      keywords = "gentoo arch testing",
+      url = "http://github.com/tom111/tatt",   # project home page
       )
