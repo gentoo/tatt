f3de2b745d410d5361717a95a629dc21fa4b2fea
Thomas Kahle <tom111@gmx.de>
2011-09-22 01:39:43 +0100

    tattConfig Object

---
+++ tatt/tattConfig.py
@@ -0,0 +1,33 @@
+"""Abstraction of a tatt configuration"""
+
+import os
+
+# from configobj
+from configobj import ConfigObj
+from validate  import Validator
+# To access the specfile:
+from pkg_resources import resource_filename
+
+# resource_filename will give us platform-independent access to the specfile
+specfile = resource_filename('tatt', 'dot-tatt-spec')
+
+# this validator will also do type conversion according to the spec file!
+class tattConfig (ConfigObj):
+    """Nothing special here, just a checked ConfigObj"""
+    def __init__(self):
+        # Read the config from ~/.tatt and create ConfigObj
+        ConfigObj.__init__(self, os.path.join(os.path.expanduser("~"), ".tatt"), configspec=specfile)
+
+        # Validate against the specfile
+        validator = Validator()
+        result = self.validate(validator)
+        
+        if result != True:
+            print "Config file validation failed!"
+            print "The following items could not be parsed"
+            for k in result.keys():
+                if result[k] == False:
+                    print k
+            sys.exit(1)
+
+
