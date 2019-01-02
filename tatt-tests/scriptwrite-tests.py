f84663baa99dfd4883a022ec57fb1169a70d04b5
Virgil Dupras <vdupras@gentoo.org>
2019-01-02 11:43:01 -0500

    Remove (broken) tatt-tests
    This has no meaning, doesn't work (non-relative import style) and hasn't
    in years (since 888142961ab804ae10ffefe354bd1e5c6a1e5a1f to be exact).
    Why keep it there?

--- tatt-tests/scriptwrite-tests.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('../tatt')
-
-from scriptwriter import *
-from gentooPackage import gentooPackage as gP
-
-print(useCombiTestString(gP("=media-sound/amarok-2.3.1-r2"),[]))
