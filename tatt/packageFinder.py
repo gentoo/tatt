f36367445c64e28200a3fbb8ef7be4295cabeba9
Sam James <sam@gentoo.org>
2020-05-02 05:25:35 +0000

    package lists: Use nattka to parse keywording bugs
    NATTkA is now used for most of the package lists on Bugzilla.
    Use it to parse the lists because currently we cannot handle
    package atoms with no version in keywording bugs.
    
    Closes: https://github.com/gentoo/tatt/issues/66
    Closes: https://github.com/gentoo/tatt/issues/65
    Signed-off-by: Sam James <sam@gentoo.org>

--- tatt/packageFinder.py
+++ tatt/packageFinder.py
@@ -1,17 +1,28 @@
 """module for extracting packages from a package/architecture list """
-
+import subprocess
 from .gentooPackage import gentooPackage as gP
 
-def findPackages (s, arch):
+def findPackages (s, arch, repo, bugnum):
     """ Given a string s,
         and a string arch
         return all gentooPackages from that string that need actioning on that arch """
 
     packages = []
 
-    for line in s.splitlines():
+    if bugnum:
+            print("Using Nattka to process the bug")
+            output = subprocess.check_output(['nattka', '--repo', repo, 'apply', '-a', arch, '-n', bugnum, '--ignore-sanity-check', '--ignore-dependencies'])
+            output = output.decode("utf8").split("\n")
+            output = [line for line in output if not line.startswith("#")]
+            output = [line.split(" ")[0] for line in output]
+    else:
+            print("Manually processing")
+            output = s.splitlines()
+
+    for line in output:
         if not line:
             continue
+
         atom, _, arches = line.replace('\t', ' ').partition(' ')
         archlist = arches.split(' ')
         if not arches or arch in archlist or ('~' + arch) in archlist:
