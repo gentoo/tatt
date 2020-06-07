042010b840bd920cece9ef00d06db392f142a7b3
Sam James <sam@gentoo.org>
2020-06-07 01:15:09 +0000

    tool: Refactor repodir handling
    * Use given repodir in config if not blank
    * Ditch ~/gentoo-x86 default (it's ancient)
      and error out if it doesn't exist
    * If no repodir given, guess:
    	* /var/db/repos/gentoo, and then
    	* /usr/portage
    
    Now that we use nattka, we need a working repodir.
    Try some sensible defaults if the given one doesn't
    work.
    
    tatt: Support file-only jobs via Nattka
    
    Signed-off-by: Sam James <sam@gentoo.org>

--- tatt/tool.py
+++ tatt/tool.py
@@ -1,3 +1,5 @@
+import os
+
 """ Helper functions used in tatt"""
 
 ## Getting unique elements of a list ##
@@ -17,3 +19,21 @@ def unique(seq, idfun=None):
             seen[marker] = 1
             result.append(item)
     return result
+
+def get_repo_dir(repodir):
+    # Prefer the repo dir in the config
+    if repodir:
+        if os.path.isdir(repodir):
+            return repodir
+        else:
+            raise ValueError("Repo dir does not seem to be a directory")
+
+    # No path given in config
+    if os.path.isdir("/var/db/repos/gentoo/"):
+        print("Using /var/db/repos/gentoo/ as fallback")
+        return "/var/db/repos/gentoo"
+    elif os.path.isdir("/usr/portage/"):
+        print("Using /usr/portage/ as fallback")
+        return "/usr/portage/"
+
+    raise ValueError("Repo dir not given and fallbacks failed")
