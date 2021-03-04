gentoo/tatt

commit eafc7c666d773cfd3b8295474a24cb6eef0919ce

templates/tatt_functions.sh: Don't use binpkgs when running the build test
This should force building the package even if
EMERGE_DEFAULT_OPTS has --usepkg=y or --getbinpkg=y, or if
FEATURES=getbinpkg.

Signed-off-by: John Helmert III <jchelmert3@posteo.net>
Closes: https://github.com/gentoo/tatt/pull/71
Signed-off-by: Sam James <sam@gentoo.org>

templates/tatt_functions.sh
@@ -55,7 +55,7 @@ function tatt_test_pkg
     TFEATURES="${FEATURES}"
   fi
 
-  eout=$( FEATURES="${TFEATURES}" emerge -1 ${TATT_EMERGEOPTS} "${1:?}" 2>&1 1>/dev/tty )
+  eout=$( FEATURES="${TFEATURES}" emerge -1 --getbinpkg=n --usepkg-exclude="${1:?}" ${TATT_EMERGEOPTS} "${1:?}" 2>&1 1>/dev/tty )
   if [[ $? == 0 ]] ; then
     if [ -n "${TFEATURES}" ]; then
       echo -n "FEATURES='${TFEATURES}' " >> "${TATT_REPORTFILE}"

commit 96a87558e37bc639a90ad17d4ffe07fc4bb2f30f

templates/tatt_functions.sh: pass package name (not atom) to --usepkg-exclude
Fixes: eafc7c666d773cfd3b8295474a24cb6eef0919ce
Fixes: https://github.com/gentoo/tatt/issues/72
Signed-off-by: Sam James <sam@gentoo.org>

templates/tatt_functions.sh
@@ -55,7 +55,9 @@ function tatt_test_pkg
     TFEATURES="${FEATURES}"
   fi
 
-  eout=$( FEATURES="${TFEATURES}" emerge -1 --getbinpkg=n --usepkg-exclude="${1:?}" ${TATT_EMERGEOPTS} "${1:?}" 2>&1 1>/dev/tty )
+  local name=$(portageq pquery "${1:?}" -n)
+
+  eout=$( FEATURES="${TFEATURES}" emerge -1 --getbinpkg=n --usepkg-exclude="${name}" ${TATT_EMERGEOPTS} "${1:?}" 2>&1 1>/dev/tty )
   if [[ $? == 0 ]] ; then
     if [ -n "${TFEATURES}" ]; then
       echo -n "FEATURES='${TFEATURES}' " >> "${TATT_REPORTFILE}"