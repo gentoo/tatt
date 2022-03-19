29f6546442763d8290bef1e17abd4107295c3e96
Arthur Zamarin <arthurzam@gentoo.org>
2022-03-19 13:26:41 +0200

    Call nattka's API directly
    By directly calling nattka's API, we have more precise results, and
    more precise error messages (with full stack trace and more
    organized output).
    
    Resolves: https://github.com/gentoo/tatt/issues/76
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- tatt/packageFinder.py
+++ tatt/packageFinder.py
@@ -1,5 +1,4 @@
 """module for extracting packages from a package/architecture list """
-import subprocess
 from .gentooPackage import gentooPackage as gP
 
 def findPackages (s, arch, repo, bugnum=False):
@@ -7,25 +6,35 @@ def findPackages (s, arch, repo, bugnum=False):
         an integer bugnum, return all gentooPackages from that string
         that need actioning on that arch """
 
-    packages = []
-
     if bugnum:
-            print("Using Nattka to process the bug")
-            output = subprocess.check_output(['nattka', '--repo', repo, 'apply', '-a', arch.replace("~", ""), '-n', bugnum, '--ignore-sanity-check', '--ignore-dependencies'])
-            output = output.decode("utf8").split("\n")
-            output = [line for line in output if not line.startswith("#")]
-            output = [line.split(" ")[0] for line in output]
+        from nattka.bugzilla import NattkaBugzilla
+        from nattka.package import find_repository, match_package_list, PackageListEmpty
+        from pathlib import Path
+
+        _, repo = find_repository(Path(repo))
+        nattka_bugzilla = NattkaBugzilla(api_key=None)
+        arch = arch.replace("~", "")
+        try:
+            bug = next(iter(nattka_bugzilla.find_bugs(
+                bugs=[int(bugnum)],
+                unresolved=True,
+                sanity_check=[True],
+                cc=f'{arch}@gentoo.org',
+            ).values()))
+            pkgs = dict(match_package_list(repo, bug, only_new=True, filter_arch=[arch], permit_allarches=True)).keys()
+            return [gP(f'{pkg.category}/{pkg.PF}') for pkg in pkgs]
+        except PackageListEmpty:
+            return []
     else:
-            print("Manually processing")
-            output = s.splitlines()
+        print("Manually processing")
+        packages = []
+        for line in s.splitlines():
+            if not line:
+                continue
 
-    for line in output:
-        if not line:
-            continue
+            atom, _, arches = line.replace('\t', ' ').partition(' ')
+            archlist = arches.split(' ')
+            if not arches or arch in archlist or ('~' + arch) in archlist:
+                packages.append(gP(atom))
 
-        atom, _, arches = line.replace('\t', ' ').partition(' ')
-        archlist = arches.split(' ')
-        if not arches or arch in archlist or ('~' + arch) in archlist:
-            packages.append(gP(atom))
-
-    return(packages)
+        return(packages)
