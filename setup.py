19ad4fb89c8939f5d0f993536079870cec4c025d
Sam James <sam@gentoo.org>
2022-10-12 20:36:35 +0100

    setup.py: 0.10
    Signed-off-by: Sam James <sam@gentoo.org>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "tatt",
-      version = "0.9",
+      version = "0.10",
       packages = find_packages(),
       scripts = ['scripts/tatt'],
       package_data = {
